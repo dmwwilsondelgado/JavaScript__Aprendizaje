b1bb787d23e65899d5a22a71f582ac73343ed322
wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-11-22 11:32:34 -0500

    fin clase bucles

---
+++ bucles.js
@@ -0,0 +1,31 @@
+
+// inicializador contador post incremento
+for(let i = 1; i<=20;i++){
+    console.log(i);
+}
+
+console.log("----------------------------");
+
+for(let i = 2; i<=40;i++){
+    console.log(i++);
+}
+console.log("----------------------------");
+
+
+
+function primos(){
+    for(let i=2;i<= 10;i){
+        i = 0;
+        let cont = 2;
+        if
+    }
+}
+
+// let cantidad = 100,j=2;
+// for(var i=2;i<cantidad;i++) {      
+//     for(;j<cantidad;j++) {        
+//         if(j%i==0 && (i==j || i==1)) {          
+//             console.log(j);          
+//         }         
+//     }            
+// }
