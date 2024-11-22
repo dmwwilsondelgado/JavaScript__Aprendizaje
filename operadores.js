0fb7d9dba9c0acaab509b7248caa2e69d35226bb
wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-11-22 10:12:47 -0500

    operadores logicos

--- operadores.js
+++ operadores.js
@@ -1,5 +1,3 @@
-let a = 5;
-let b = 250;
 
 // console.log(typeof a);
 
@@ -7,4 +5,52 @@ let b = 250;
 //    console.log(a - b);
 // }
 
-let resultado = a + (b + (b - a) * a) / c ;
+// let resultado = a + (b + (b - a) * a) / c ;
+
+// Operadores Loggicos
+
+// (<) menor que
+//(>) mayor q 
+//(==)igual
+//(===) extrictamente igual y unico en su tipo
+// (!=)negacion
+
+// let g = 5;
+// let h = 10;
+
+// console.log(a < b);
+// let i = 0;
+// console.log(i++);
+
+
+// soperadores logicos
+
+let a = 5;
+let b = 10;
+console.log(5 > 7 || false);
+console.log( !false && true);
+
+// ANNIDACION
+
+// OPERADOR TERNIARIO
+
+
+// switch(dia){
+//     case 1:
+//         "lunes";
+//     case 2:
+//         "martes";
+//     case 3:
+//         "miercoles";
+//     case 4:
+//         "jueves";
+//     case 5:
+//         "viernes";
+//     case 6:
+//         "sabado";
+//     case 7:
+//         "diomingo";
+//     break
+// }
+
+
